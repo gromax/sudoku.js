b109066b891d1433e0af4304494acb9a553f4de1
Maxence Klein <maxence.klein@goupill.fr>
2024-07-22 21:28:59 +0200

    constantes globales

---
+++ src/constantes.js
@@ -0,0 +1,12 @@
+const CELLSIZE = 100;
+const GRIDSIZE = 9;
+const SUBGRIDSIZE = 3;
+
+const DIRECTION = {
+    UP    : 1,
+    DOWN  : 2,
+    LEFT  : 3,
+    RIGHT : 4
+ }
+
+export { CELLSIZE, GRIDSIZE, SUBGRIDSIZE, DIRECTION };
