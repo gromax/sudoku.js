9538afedf44eae2fa5e9724d1e7534c188119dfa
Maxence Klein <maxence.klein@goupill.fr>
2024-07-22 23:03:02 +0200

    attaché à l'objet de sélection

--- src/graphic/cell.js
+++ src/graphic/cell.js
@@ -7,15 +7,25 @@ class GCell {
     static STROKE = { color: '#000', width: 2 };
     #group;
     #back;
-    constructor(parent) {
+    #line;
+    #col;
+    constructor(parent, line, col) {
         this.#group = parent.nested();
         this.#back = this.#group.rect(CELLSIZE, CELLSIZE);
         this.#back.fill(GCell.FILL).stroke(GCell.STROKE);
+        this.#line = line;
+        this.#col = col;
     }
 
     move(line, col) {
         this.#group.move(col*CELLSIZE, line*CELLSIZE);
         return this;
+    }
+
+    attachSelector(selector) {
+        let line = this.#line;
+        let col = this.#col;
+        this.#back.click(function(){ selector.clickCell(line, col); });
     }
 }
 
