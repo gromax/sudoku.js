a29954070979f6539359fb61576a14712ce2ff36
Maxence Klein <maxence.klein@goupill.fr>
2024-07-22 23:03:39 +0200

    permet le tracer

--- src/graphic/selection.js
+++ src/graphic/selection.js
@@ -1,61 +1,90 @@
 import { SVG } from '@svgdotjs/svg.js';
-import { GRIDSIZE } from '../constantes';
+import { CELLSIZE, GRIDSIZE } from '../constantes';
+import { Contour } from '../utils/contour';
 
 class Selection {
-    static FILL = { color:"#1d6ba3", opacity:"0.5" };
-    static BORDER = { width:5, color:"#2a5a7d" };
+    static STROKE = { width:10, color:"#0066ff" };
 
     #group;
-    #cells;
+    #states;
     constructor(parent) {
         this.#group = parent.nested();
-        this.#cells = Array();
+        this.#states = Array(GRIDSIZE*GRIDSIZE).fill(false);
     }
 
     #draw() {
-        
+        let coords = [];
+        for (let i=0; i<GRIDSIZE; i++) {
+            for (let j=0; j<GRIDSIZE; j++) {
+                if (this.#states[i*GRIDSIZE+j]) {
+                    coords.push([i,j]);
+                }
+            }
+        }
+        let c = new Contour(coords);
+        let paths = c.getPaths(CELLSIZE, 5);
+        for (let p of paths) {
+            this.#group.polygon(p).stroke(Selection.STROKE).fill('none');
+        }
     }
 
     clear() {
-        this.#cells = Array();
+        this.#states = Array(GRIDSIZE*GRIDSIZE).fill(false);
         this.#group.clear();
         return this;
     }
 
     remove(line, col) {
         if ((line<0) || (line>=GRIDSIZE)){
-            return this;
+            return;
         }
         if ((col<0) || (col>=GRIDSIZE)){
-            return this;
+            return;
         }
         let index = line*GRIDSIZE+col;
-        if (this.#cells.indexOf(index)<0){
-            return this;
+        if (!this.#states[index]){
+            return;
         }
-        this.#cells.remove(index);
+        this.#states[index] = false;
         this.#group.clear();
         this.#draw();
-        return this;
     }
 
     add(line, col) {
         if ((line<0) || (line>=GRIDSIZE)){
-            return this;
+            return;
         }
         if ((col<0) || (col>=GRIDSIZE)){
-            return this;
+            return;
         }
         let index = line*GRIDSIZE+col;
-        if (this.#cells.indexOf(index)>=0){
-            return this;
+        if (this.#states[index]){
+            return;
         }
-        this.#cells.push(index);
+        this.#states[index] = true;
+        this.#group.clear();
+        this.#draw();
+    }
+
+    toggle(line, col) {
+        if ((line<0) || (line>=GRIDSIZE)){
+            return;
+        }
+        if ((col<0) || (col>=GRIDSIZE)){
+            return;
+        }
+        let index = line*GRIDSIZE+col;
+        this.#states[index] = !this.#states[index];
         this.#group.clear();
         this.#draw();
         return this;
     }
 
+    clickCell(line, col) {
+        this.toggle(line, col);
+    }
 
 
 }
+
+export { Selection };
