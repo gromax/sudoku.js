f954e4ca75faad75de8812b26cc64a21c636eacc
Maxence Klein <maxence.klein@goupill.fr>
2024-08-17 00:16:33 +0200

    Essai avec des commandes variées

---
+++ src/index.js
@@ -0,0 +1,13 @@
+//import { SVG } from '@svgdotjs/svg.js';
+//import { GGrid } from './graphic/grid';
+//import { Selection } from './graphic/selection';
+
+import { Board } from "./graphic/board";
+
+var board = new Board('#board', '9x9S:ThBEBGEGED:Y;LiBbFBee:b;DcEFFCFB:p-;Dcffgfgh:o{2÷};lala;liCdEdEf:b;8FH:r;5BE:w');
+
+//var selector = new Selection(board);
+//board.click(function(e){ selector.clickEvent(e); });
+
+//$('#selection').on("click", function(e){ selector.selectMode($(this).is(':checked'));})
+//$('#border').on("click", function(e){ selector.selectBorderMode($(this).is(':checked'));})
