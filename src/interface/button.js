880f4954c7434b8df0478004d0bfa018bda0f8c4
Maxence Klein <maxence.klein@goupill.fr>
2025-01-01 18:25:55 +0100

    création

---
+++ src/interface/button.js
@@ -0,0 +1,224 @@
+import { SVG } from '@svgdotjs/svg.js';
+import { Canvas } from '../graphic/canvas';
+
+class Button {
+    static STROKE = { width:10, color:"#888888" };
+    static STROKEON = { width:10, color:"#FF8888" };
+    static FILL   = { color:"#BBBBBB" };
+    static FILLDARKER   = { color:"#888888" };
+    static FILLON = { color:"#EEBBBB" };
+    static FILLOVER   = { color:"#CCCCCC" };
+    static RADIUS = 0.1;
+    /** @type {Canvas} */
+    #canvas;
+    /** @type {SVG.G} */
+    #group;
+    /** @type {SVG.Rect} */
+    #line;
+    /** @type {Number} */
+    #col;
+    /** @type {boolean} */
+    #selected;
+    /** @type {boolean} */
+    #bistable = false;
+    /** @type {Function} */
+    #callBack = null;
+    /** @type {string} */
+    #tag = "";
+
+    /**
+     * constructeur
+     * @param {Canvas} parent
+     * @param {Number} line
+     * @param {Number} col
+     * @param {string} tag
+     */
+    constructor(parent, line, col, tag) {
+        this.#tag = tag;
+        this.#canvas = parent;
+        this.#group = this.#canvas.group();
+        let square = this.#canvas.square(line+.05, col+.05, .9, Button.RADIUS);
+        square.fill(Button.FILL).stroke(Button.STROKE);
+        this.#group.add(square);
+        let button = this.#canvas.square(line, col, 1, 0);
+        button.fill({color:'#FFFFFF', opacity:"0.01"}).stroke('none');
+        this.#group.add(button);
+        this.#line = line;
+        this.#col = col;
+        this.#selected = false;
+
+        let self = this;
+        button.mouseover(function(e){
+            self.mouseover(e);
+        });
+
+        button.mouseout(function(e){
+            self.mouseout(e);
+            
+        });
+
+        button.mousedown(function(e){
+            self.mousedown(e);
+        });
+
+        button.mouseup(function(e){
+            self.mouseup(e);
+        });
+    }
+
+    /**
+     * proxy pour evénement mouseover
+     * @param {Event} e 
+     */
+    mouseover(e) {
+        let square = this.#group.children()[0];
+        if ((e.buttons==1)||this.selected) {
+            square.fill(Button.FILLON);
+            square.stroke(Button.STROKEON);
+        } else {
+            square.fill(Button.FILLOVER);
+        }
+    }
+
+    /**
+     * proxy pour événement mouseout
+     * @param {Event} e 
+     */
+    mouseout(e) {
+        if (!this.selected){
+            let square = this.#group.children()[0];
+            square.fill(Button.FILL);
+            square.stroke(Button.STROKE);
+        }
+    }
+
+    /**
+     * proxy pour événement mousedown
+     * @param {Event} e 
+     */
+    mousedown(e){
+        let square = this.#group.children()[0];
+        square.fill(Button.FILLON);
+        square.stroke(Button.STROKEON);
+    }
+
+    /**
+     * proxy pour événement mouseup
+     * @param {Event} e 
+     */
+    mouseup(e){
+        if (this.#callBack != null) {
+            this.#callBack(this, e);
+        }
+        if (this.bistable && !this.selected){
+            this.setSelected(true);
+        } else {
+            this.setSelected(false);
+        }
+    }
+
+    /**
+     * trace un carré de couleur sur le bouton
+     * @param {object|string} fill 
+     * @param {Number} xoffset
+     * @param {Number} yoffset
+     * @param {Number} width
+     * @returns {Button}
+     */
+    drawSquare(fill, xoffset, yoffset, size){
+        if (typeof fill == 'string') {
+            fill = {'color':fill};
+        }
+        let s = this.#canvas.square(this.#line+yoffset, this.#col+xoffset, size);
+        s.stroke('none').fill(fill);
+        this.#group.add(s);
+        s.backward();
+        return this;
+    }
+
+    /**
+     * trace un carré de couleur sur le bouton
+     * @param {string} text
+     * @param {Number} xoffset
+     * @param {Number} yoffset
+     * @param {string} color
+     * @returns {Button}
+     */
+    drawText(text, xoffset, yoffset, color = "#000000"){
+        let t = this.#canvas.text(text, [this.#col + xoffset, this.#line + yoffset], 0.5);
+        t.stroke('none').fill({'color':color});
+        this.#group.add(t);
+        t.backward();
+        return this;
+    }
+
+    /**
+     * Accesseur vers selected
+     * @returns {boolean}
+     */
+    get bistable() {
+        return this.#bistable;
+    }
+
+    /**
+     * Accesseur vers tag
+     * @returns {string}
+     */
+    get tag() {
+        return this.#tag;
+    }
+
+    /**
+     * Accesseur vers selected
+     * @returns {boolean}
+     */
+    get selected() {
+        return this.#selected;
+    }
+
+    /**
+     * modifie valeur de selected
+     * @param {boolean} value 
+     */
+    setSelected(value){
+        this.#selected = value;
+        let square = this.#group.children()[0];
+        if (this.#selected) {
+            square.fill(Button.FILLON);
+            square.stroke(Button.STROKEON);
+        } else {
+            square.stroke(Button.STROKE);
+            square.fill(Button.FILL);
+        }
+    }
+
+    /**
+     * éteint le bouton
+     */
+    unselect() {
+        this.setSelected(false);
+    }
+
+    /**
+     * assigne la fonction callBack
+     * @param {Function|null} callBack
+     * @returns {Button}
+     */
+    assignCallBack(callBack){
+        this.#callBack = callBack;
+        return this;
+    }
+
+    /**
+     * rend le bouton bistable
+     * @returns {Button}
+     */
+    setBistable() {
+        this.#bistable = true;
+        return this;
+    }
+
+
+}
+
+export { Button };
---
+++ src/interface/pad.js
@@ -0,0 +1,101 @@
+import { SVG } from "@svgdotjs/svg.js";
+import { Canvas } from "../graphic/canvas";
+import { Button } from "./button";
+
+
+class Radio {
+    /** @type {Array} */
+    #buttons;
+
+    /**
+     * constructeur
+     * @param {Array} buttons 
+     */
+    constructor(buttons) {
+        this.#buttons = buttons;
+        if (buttons.length == 0) {
+            throw new Error('Liste de boutons vide dans Radio !');
+        }
+        let f = function(buttonClicked, event) {
+            for (let b of buttons) {
+                if (b!=buttonClicked) {
+                    b.unselect();
+                }
+            }
+        }
+        buttons[0].setSelected(true);
+        for (let b of buttons) {
+            b.setBistable();
+            b.assignCallBack(f);
+        }
+    }
+
+    /**
+     * Accesseur vers selected
+     * @returns {string}
+     */
+    get selected() {
+        for (let b of this.#buttons) {
+            if (b.selected) {
+                return b.tag;
+            }
+        }
+        throw new Error('Aucun bouton validé !');
+    }
+
+}
+
+class Pad {
+    static HEIGHT = 400;
+    static WIDTH = 1000;
+    static BUTTONSIZE = 80;
+
+
+    /** @type {SVG.SVG} */
+    #content;
+    /** @type {Canvas} */
+    #canvas;
+
+
+    /**
+     * constructeur
+     * @param {string} id identifiant dom du conteneur
+     */
+    constructor(id) {
+        this.#content = SVG().addTo(id).size(Pad.WIDTH, Pad.HEIGHT);
+        this.#canvas = new Canvas(this.#content, Pad.BUTTONSIZE, 0);
+        // exemple de bouton
+        new Button(this.#canvas, 0, 0, "a");
+        new Button(this.#canvas, 0, 1, "b");
+        new Button(this.#canvas, 0, 2, "c");
+        new Button(this.#canvas, 0, 3, "d");
+
+        let radioPosition = new Radio([
+            (new Button(this.#canvas, 0, 5, "nw")).drawSquare(Button.FILLDARKER, 0.2, 0.2, 0.3),
+            (new Button(this.#canvas, 0, 6, "n")).drawSquare(Button.FILLDARKER, 0.35, 0.2, 0.3),
+            (new Button(this.#canvas, 0, 7, "ne")).drawSquare(Button.FILLDARKER, 0.5, 0.2, 0.3),
+            (new Button(this.#canvas, 1, 5, "w")).drawSquare(Button.FILLDARKER, 0.2, 0.35, 0.3),
+            (new Button(this.#canvas, 1, 6, "c")).drawSquare(Button.FILLDARKER, 0.35, 0.35, 0.3),
+            (new Button(this.#canvas, 1, 7, "e")).drawSquare(Button.FILLDARKER, 0.5, 0.35, 0.3),
+            (new Button(this.#canvas, 2, 5, "sw")).drawSquare(Button.FILLDARKER, 0.2, 0.5, 0.3),
+            (new Button(this.#canvas, 2, 6, "s")).drawSquare(Button.FILLDARKER, 0.35, 0.5, 0.3),
+            (new Button(this.#canvas, 2, 7, "se")).drawSquare(Button.FILLDARKER, 0.5, 0.5, 0.3)
+        ]);
+
+        let radioColor = new Radio([
+            (new Button(this.#canvas, 0, 9, "#4287f5")).drawSquare("#4287f5", 0.3, 0.3, 0.4),
+            (new Button(this.#canvas, 0, 10, "#d42215")).drawSquare("#d42215", 0.3, 0.3, 0.4),
+            (new Button(this.#canvas, 0, 11, "#0be629")).drawSquare("#0be629", 0.3, 0.3, 0.4),
+            (new Button(this.#canvas, 1, 9, "#f2ee07")).drawSquare("#f2ee07", 0.3, 0.3, 0.4),
+            (new Button(this.#canvas, 1, 10, "#000000")).drawSquare("#000000", 0.3, 0.3, 0.4),
+            (new Button(this.#canvas, 1, 11, "#8a8a8a")).drawSquare("#8a8a8a", 0.3, 0.3, 0.4),
+            (new Button(this.#canvas, 2, 9, "#f58a07")).drawSquare("#f58a07", 0.3, 0.3, 0.4),
+            (new Button(this.#canvas, 2, 10, "#eb42df")).drawSquare("#eb42df", 0.3, 0.3, 0.4),
+            (new Button(this.#canvas, 2, 11, "#b207f5")).drawSquare("#b207f5", 0.3, 0.3, 0.4)
+        ]);
+
+       
+    }
+}
+
+export { Pad };
