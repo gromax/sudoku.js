fda641384b573965b494773b32fb434c039dbe4c
Maxence Klein <maxence.klein@goupill.fr>
2025-01-01 18:24:45 +0100

    ajout pad

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@
 //import { Selection } from './graphic/selection';
 
 import { Board } from "./graphic/board";
+import { Pad } from "./interface/pad";
 
 var board = new Board('#board', '9x9S', 'ThBEBGEGED:Y;LibBIBIFgF:b;CagEFFCFB:py-;Cagffgfgh:o:20{2÷};lala;liCdEdEf:b;8FH:r;5BE:w;Tag{>}Gd:r.Cs80rR;CagJIJJIJ:gv:0=;ColBIBJCJ:v.50');
+var pad = new Pad("#pad");
+
 
 //var selector = new Selection(board);
 //board.click(function(e){ selector.clickEvent(e); });
