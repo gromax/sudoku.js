e02943cdc1998a062b3880064aebabb0ba125068
Maxence Klein <maxence.klein@goupill.fr>
2025-01-02 16:44:07 +0100

    ANCRES déplacé dans les constantes

--- src/constantes.js
+++ src/constantes.js
@@ -9,4 +9,16 @@ const DIRECTION = {
     RIGHT : 4
  }
 
-export { CELLSIZE, GRIDSIZE, SUBGRIDSIZE, DIRECTION };
+const ANCRES = {
+    "N" : {x:0.5, y:0,   clockwise:"E",  counterclockwise:"W" },
+    "NE": {x:1,   y:0,   clockwise:"SE", counterclockwise:"NW"},
+    "NW": {x:0,   y:0,   clockwise:"NE", counterclockwise:"SW"},
+    "E" : {x:1,   y:0.5, clockwise:"S",  counterclockwise:"N" },
+    "SE": {x:1,   y:1,   clockwise:"SW", counterclockwise:"NE"},
+    "S" : {x:0.5, y:1,   clockwise:"W",  counterclockwise:"E" },
+    "SW": {x:0,   y:1,   clockwise:"NW", counterclockwise:"SE"},
+    "W" : {x:0,   y:0.5, clockwise:"N",  counterclockwise:"S" },
+    "C" : {x:0.5, y:0.5, clockwise:"C",  counterclockwise:"C" },
+}
+
+export { CELLSIZE, GRIDSIZE, SUBGRIDSIZE, DIRECTION, ANCRES };
