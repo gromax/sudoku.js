ba8f2a32238dfeed4f0c0778db75e34f8a77277f
Maxence Klein <maxence.klein@goupill.fr>
2025-01-05 21:02:00 +0100

    premier commit

---
+++ src/graphic/borders.js
@@ -0,0 +1,248 @@
+import { SVG } from '@svgdotjs/svg.js';
+import { Canvas } from './canvas';
+import { Coords } from '../utils/coords';
+
+
+class Junction {
+    static RADIUS = 0.05;
+    static DEFAULTFILL = '#000000';
+    /** @type {SVG.Line} */
+    #node;
+
+    /** @type {Array<Segment>} */
+    #edges;
+
+    /**
+     * constructeur
+     * @param {Canvas} canvas 
+     * @param {number} line 
+     * @param {number} col 
+     */
+    constructor(canvas, line, col) {
+        this.#node = canvas.disc(line, col, Junction.RADIUS);
+        this.#node.fill('none').stroke('none');
+        this.#edges = [];
+    }    
+
+    /**
+     * assigne un segment attaché à la jonction
+     * @param {Segment} edge
+     * @returns {Junction}
+     */
+    attachEdge(edge) {
+        this.#edges.push(edge);
+        return this;
+    }
+
+    refresh() {
+        let colors = [];
+        for (s of this.#edges) {
+            let c = s.color;
+            if ((c == 'none')||(colors.indexOf(c) != -1)) {
+                continue;
+            }
+            colors.push(c);
+        }
+        if (colors.length == 0) {
+            this.#node.fill('none');
+        } else if (colors.length == 1) {
+            this.#node.fill(colors[0]);
+        } else {
+            this.#node.fill(Junction.DEFAULTFILL);
+        }
+    }
+
+
+}
+
+class Segment {
+    WIDTH = 2;
+    /** @type {SVG.Line} */
+    #node
+
+    /** @type {Array<Junction>} */
+    #extremity;
+
+    /** @type {string} */
+    #color;
+
+    /**
+     * constructeur
+     * @param {Canvas} canvas 
+     * @param {number} line 
+     * @param {number} col 
+     * @param {boolean} vert 
+     */
+    constructor(canvas, line, col, vert) {
+        let start = new Coords(col, line);
+        let end = vert? new Coords(col,line+1) : new Coords(col+1,line);
+        this.#node = canvas.segment(start, end);
+        this.#node.fill('none').stroke('none');
+        this.#extremity = []
+        this.#color = 'none';
+    }
+
+    /**
+     * assigne les extrémités
+     * @param {Junction} a 
+     * @returns {Segment}
+     */
+    attachJunction(a) {
+        this.#extremity.push(a);
+        a.attachEdge(this);
+        return this;
+    }
+
+    /**
+     * accesseur
+     * @returns {string}
+     */
+    get color() {
+        return this.#color;
+    }
+
+    /**
+     * assigne une couleur
+     * @param {string} color 
+     * @returns {Segment}
+     */
+    setColor(color) {
+        this.#color = color;
+        this.#node.stroke({color:color, width:Segment.WIDTH});
+        for (j of this.#extremity) {
+            j.refresh();
+        }
+        return this;
+    }
+
+    /**
+     * Bascule la couleur du segment
+     * @param {string} color
+     * @returns {Segment}
+     */
+    toggleColor(color) {
+        if (this.color == color) {
+            return this.hide();
+        } else {
+            return this.setColor(color);
+        }
+    }
+
+    /**
+     * cache le segment
+     * @returns {Segment}
+     */
+    hide() {
+        return this.setColor('none');
+    }
+
+}
+
+class Borders {
+    /** @type {number} */
+    #width
+
+    /** @type {number} */
+    #height
+
+    /** @type {Array<Segment>} */
+    #verticals
+
+    /** @type {Array<Segment>} */
+    #horizontals
+
+    /**
+     * constructeur
+     * @param {Canvas} canvas 
+     * @param {number} width 
+     * @param {number} height 
+     */
+    constructor(canvas, width, height) {
+        this.#width = width;
+        this.#height = height;
+        this.#verticals = [];
+        for (let line=0; line<height; line++) {
+            for (let col=0; col<width+1; col++){
+                this.#verticals.push(new Segment(canvas, line, col, true));
+            }
+        }
+        this.#horizontals = [];
+        for (let line=0; line<height+1; line++) {
+            for (let col=0; col<width; col++){
+                this.#horizontals.push(new Segment(canvas, line, col, false));
+            }
+        }
+        for (let col=0; col<width+1; col++){
+            for (let line=0; line<height+1; line++) {
+                let j = new Junction(canvas, line, col);
+                if (line>0) {
+                    this.#verticals[(line-1)*(this.#width+1) + col].attachJunction(j);
+                }
+                if (line<this.#height) {
+                    this.#verticals[line*(this.#width+1) + col].attachJunction(j);
+                }
+                if (col>0) {
+                    this.#horizontals[line*this.#width + col-1].attachJunction(j);
+                }
+                if (col<this.#width) {
+                    this.#horizontals[line*this.#width + col].attachJunction(j);
+                }
+            }
+        }
+    }
+
+    /**
+     * renvoie le segment supérieur
+     * @param {number} line 
+     * @param {number} col 
+     * @returns {Segment}
+     */
+    up(line, col) {
+        if ((line<0) || (line>=this.#height) || (col<0) || (col>=this.#width)) {
+            throw new Error(`line:${line} col:${col} n'est pas dans la grille !`);
+        }
+        return this.#horizontals[line*this.#width + col];
+    }
+
+    /**
+     * renvoie le segment inférieur
+     * @param {number} line 
+     * @param {number} col 
+     * @returns {Segment}
+     */
+    down(line, col) {
+        if ((line<0) || (line>=this.#height) || (col<0) || (col>=this.#width)) {
+            throw new Error(`line:${line} col:${col} n'est pas dans la grille !`);
+        }
+        return this.#horizontals[(line+1)*this.#width + col];
+    }
+
+    /**
+     * renvoie le segment de gauche
+     * @param {number} line 
+     * @param {number} col 
+     * @returns {Segment}
+     */
+    left(line, col) {
+        if ((line<0) || (line>=this.#height) || (col<0) || (col>=this.#width)) {
+            throw new Error(`line:${line} col:${col} n'est pas dans la grille !`);
+        }
+        return this.#verticals[line*(this.#width+1) + col];
+    }
+
+    /**
+     * renvoie le segment de droite
+     * @param {number} line 
+     * @param {number} col 
+     * @returns {Segment}
+     */
+    left(line, col) {
+        if ((line<0) || (line>=this.#height) || (col<0) || (col>=this.#width)) {
+            throw new Error(`line:${line} col:${col} n'est pas dans la grille !`);
+        }
+        return this.#verticals[line*(this.#width+1) + col+1];
+    }
+}
+
+
+export { Borders }
