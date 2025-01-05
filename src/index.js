c43e61bdb597a50fb5d5405d95c9679dda3faab6
Maxence Klein <maxence.klein@goupill.fr>
2025-01-05 15:04:33 +0100

    lien vers board pour pad

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@
 import { Board } from "./graphic/board";
 import { Pad } from "./interface/pad";
 
+
+
 var board = new Board('#board', '9x9S', 'ThBEBGEGED:Y;LibBIBIFgF:b;CagEFFCFB:py-;Cagffgfgh:o:20{2÷};lala;liCdEdEf:b;8FH:r;5BE:w;Tag{>}Gd:r.Cs80rR;CagJIJJIJ:gv:0=;ColBIBJCJ:v.50');
-var pad = new Pad("#pad");
+var pad = new Pad("#pad", board);
 
 
 //var selector = new Selection(board);
