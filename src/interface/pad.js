66d48152dcacfc8888d1f93e005aab5efcfa3cf1
Maxence Klein <maxence.klein@goupill.fr>
2025-01-13 13:01:23 +0100

    action des boutons eraser, ajout boutons undo/redp

--- src/interface/pad.js
+++ src/interface/pad.js
@@ -41,7 +41,11 @@ class Pad {
                 board.toggleSelColor(self.selectedColor);
             }
         );
-        (new Button(this.#canvas, 3, 8, "eraser")).addPicto("eraser");
+        (new Button(this.#canvas, 3, 8, "eraser")).addPicto("eraserc").assignCallBack(
+            function(b,e) {
+                board.clearColors();
+            }
+        );
         (new Button(this.#canvas, 0, 8, "border-left")).addPicto("left").assignCallBack(
             function(b, e){
                 board.toggleBorderColor(self.selectedColor, DIRECTION.LEFT);
@@ -67,7 +71,7 @@ class Pad {
                 board.toggleOuterBorderColor(self.selectedColor, DIRECTION.DOWN);
             }
         );
-        (new Button(this.#canvas, 0, 6, "selection")).addPicto("selection").setBistable().assignCallBack(
+        (new Button(this.#canvas, 0, 1, "selection")).addPicto("selection").setBistable().assignCallBack(
             function(b, e){
                 if (b.selected) {
                     board.setSelectionVerrou();
@@ -77,15 +81,29 @@ class Pad {
             }
         );
 
-        /*(new Button(this.#canvas, 0, 0, "1")).drawText("1", .5, .5).assignCallBack(
+        (new Button(this.#canvas, 0, 2, "eraser")).addPicto("eraser").assignCallBack(
+            function(b, e){
+                board.clearDigits(self.position);
+            }
+        );
+
+        (new Button(this.#canvas, 0, 5, "undo")).addPicto("undo").assignCallBack(
             function(b, e){
                 ;
             }
-        );*/
+        );
+
+        (new Button(this.#canvas, 0, 6, "redo")).addPicto("redo").assignCallBack(
+            function(b, e){
+                ;
+            }
+        );
+
+
         for (let i=0; i<3; i++) {
             for (let j=0; j<3; j++) {
                 let tag = `${i*3+j+1}`;
-                (new Button(this.#canvas, i+1, j, tag)).drawText(tag).assignCallBack(
+                (new Button(this.#canvas, i+1, j+1, tag)).drawText(tag).assignCallBack(
                     function(b, e){
                         board.toggleDigit(b.tag, self.position, self.selectedColor);
                     }
@@ -94,16 +112,16 @@ class Pad {
         }
 
         this.#radioPosition = new Radio([
-            (new Button(this.#canvas, 1, 5, "NW")).drawSquare(Button.FILLDARKER, 0.2, 0.2, 0.3),
-            (new Button(this.#canvas, 1, 6, "N")).drawSquare(Button.FILLDARKER, 0.35, 0.2, 0.3),
-            (new Button(this.#canvas, 1, 7, "NE")).drawSquare(Button.FILLDARKER, 0.5, 0.2, 0.3),
-            (new Button(this.#canvas, 2, 5, "W")).drawSquare(Button.FILLDARKER, 0.2, 0.35, 0.3),
-            (new Button(this.#canvas, 2, 6, "C")).drawSquare(Button.FILLDARKER, 0.35, 0.35, 0.3),
-            (new Button(this.#canvas, 2, 7, "E")).drawSquare(Button.FILLDARKER, 0.5, 0.35, 0.3),
-            (new Button(this.#canvas, 3, 5, "SW")).drawSquare(Button.FILLDARKER, 0.2, 0.5, 0.3),
-            (new Button(this.#canvas, 3, 6, "S")).drawSquare(Button.FILLDARKER, 0.35, 0.5, 0.3),
-            (new Button(this.#canvas, 3, 7, "SE")).drawSquare(Button.FILLDARKER, 0.5, 0.5, 0.3),
-            (new Button(this.#canvas, 0, 5, "P")).drawSquare(Button.FILLDARKER, 0.2, 0.2, 0.6)
+            (new Button(this.#canvas, 1, 4, "NW")).drawSquare(Button.FILLDARKER, 0.2, 0.2, 0.3),
+            (new Button(this.#canvas, 1, 5, "N")).drawSquare(Button.FILLDARKER, 0.35, 0.2, 0.3),
+            (new Button(this.#canvas, 1, 6, "NE")).drawSquare(Button.FILLDARKER, 0.5, 0.2, 0.3),
+            (new Button(this.#canvas, 2, 4, "W")).drawSquare(Button.FILLDARKER, 0.2, 0.35, 0.3),
+            (new Button(this.#canvas, 2, 5, "C")).drawSquare(Button.FILLDARKER, 0.35, 0.35, 0.3),
+            (new Button(this.#canvas, 2, 6, "E")).drawSquare(Button.FILLDARKER, 0.5, 0.35, 0.3),
+            (new Button(this.#canvas, 3, 4, "SW")).drawSquare(Button.FILLDARKER, 0.2, 0.5, 0.3),
+            (new Button(this.#canvas, 3, 5, "S")).drawSquare(Button.FILLDARKER, 0.35, 0.5, 0.3),
+            (new Button(this.#canvas, 3, 6, "SE")).drawSquare(Button.FILLDARKER, 0.5, 0.5, 0.3),
+            (new Button(this.#canvas, 0, 4, "P")).drawSquare(Button.FILLDARKER, 0.2, 0.2, 0.6)
         ]);
 
         this.#radioColor = new Radio([
