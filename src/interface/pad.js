7496621522337ab60800d05371ad6ef100dfedbf
Maxence Klein <maxence.klein@goupill.fr>
2025-01-14 23:00:05 +0100

    Tout changé pour accueillir gestionnaire d'événement, création boutons plus simple

--- src/interface/pad.js
+++ src/interface/pad.js
@@ -2,10 +2,8 @@ import { SVG } from "@svgdotjs/svg.js";
 import { Canvas } from "../graphic/canvas";
 import { Button } from "./button";
 import { Radio } from "./radio";
-import { Board } from "../graphic/board";
 import { DIRECTION, COLORS } from "../constantes";
-
-
+import { Events } from "../utils/events";
 
 class Pad {
     static HEIGHT = 400;
@@ -25,115 +23,138 @@ class Pad {
     /** @type {Radio} */
     #radioPosition;
 
-
     /**
      * constructeur
      * @param {string} id identifiant dom du conteneur
-     * @param {Board} board
+     * @param {Events} eventsGest gestionnaire d'événements
      */
-    constructor(id, board) {
+    constructor(id, eventsGest) {
         let self = this;
         this.#content = SVG().addTo(id).size(Pad.WIDTH, Pad.HEIGHT);
         this.#canvas = new Canvas(this.#content, Pad.BUTTONSIZE, 0);
         // exemple de bouton
-        (new Button(this.#canvas, 2, 8, "paint")).addPicto("paint").assignCallBack(
-            function(b, e){
-                board.toggleSelColor(self.selectedColor);
-            }
-        );
-        (new Button(this.#canvas, 3, 8, "eraser")).addPicto("eraserc").assignCallBack(
-            function(b,e) {
-                board.clearColors();
-            }
-        );
-        (new Button(this.#canvas, 0, 8, "border-left")).addPicto("left").assignCallBack(
-            function(b, e){
-                board.toggleBorderColor(self.selectedColor, DIRECTION.LEFT);
-            }
-        );
-        (new Button(this.#canvas, 0, 9, "border-top")).addPicto("top").assignCallBack(
-            function(b, e){
-                board.toggleBorderColor(self.selectedColor, DIRECTION.UP);
-            }
-        );
-        (new Button(this.#canvas, 0, 10, "border-right")).addPicto("right").assignCallBack(
-            function(b, e){
-                board.toggleBorderColor(self.selectedColor, DIRECTION.RIGHT);
-            }
-        );
-        (new Button(this.#canvas, 0, 11, "border-bottom")).addPicto("bottom").assignCallBack(
-            function(b, e){
-                board.toggleBorderColor(self.selectedColor, DIRECTION.DOWN);
-            }
-        );
-        (new Button(this.#canvas, 1, 8, "border-outer")).addPicto("outer").assignCallBack(
-            function(b, e){
-                board.toggleOuterBorderColor(self.selectedColor, DIRECTION.DOWN);
-            }
-        );
-        (new Button(this.#canvas, 0, 1, "selection")).addPicto("selection").setBistable().assignCallBack(
-            function(b, e){
-                if (b.selected) {
-                    board.setSelectionVerrou();
-                } else {
-                    board.resetSelectionVerrou();
-                }
-            }
-        );
+        new Button(this.#canvas, 2, 8, eventsGest, {
+            "event": "paintClick",
+            "picto":"paint"
+        });
+        eventsGest.addEvent("paintClick", function(e, data){
+            eventsGest.triggerEvent("paint", e, {color: self.selectedColor});
+        });
 
-        (new Button(this.#canvas, 0, 2, "eraser")).addPicto("eraser").assignCallBack(
-            function(b, e){
-                board.clearDigits(self.position);
-            }
-        );
+        new Button(this.#canvas, 3, 8, eventsGest, {
+            "event": "eraseColorClick",
+            "picto":"eraserc"
+        });
+        eventsGest.addEvent("eraseColorClick", function(e, data){
+            eventsGest.triggerEvent("paint", e, null);
+        });
 
-        (new Button(this.#canvas, 0, 5, "undo")).addPicto("undo").assignCallBack(
-            function(b, e){
-                ;
-            }
-        );
+        new Button(this.#canvas, 0, 8, eventsGest, {
+            "event": "borderClick",
+            "picto":"left",
+            "data": DIRECTION.LEFT
+        });
+        new Button(this.#canvas, 0, 9, eventsGest, {
+            "event": "borderClick",
+            "picto":"top",
+            "data":DIRECTION.UP
+        });
+        new Button(this.#canvas, 0, 10, eventsGest, {
+            "event":"borderClick",
+            "picto":"right",
+            "data":DIRECTION.RIGHT
+        });
+        new Button(this.#canvas, 0, 11, eventsGest, {
+            "event":"borderClick",
+            "picto":"bottom",
+            "data":DIRECTION.DOWN
+        });
+        new Button(this.#canvas, 1, 8, eventsGest, {
+            "event":"borderClick",
+            "picto":"outer",
+            "data":-1
+        });
 
-        (new Button(this.#canvas, 0, 6, "redo")).addPicto("redo").assignCallBack(
-            function(b, e){
-                ;
-            }
-        );
+        eventsGest.addEvent("borderClick", function(e, data){
+            eventsGest.triggerEvent("border", e, {
+                color: self.selectedColor,
+                direction: data.data
+            });
+        });        
 
+        new Button(this.#canvas, 0, 1, eventsGest, {
+            "event":"selectionClick",
+            "picto":"selection",
+            "bistable": true
+        });
+        eventsGest.addEvent("selectionClick", function(e, data){
+            if (!data) {
+                throw new Error("data n'est pas défini !");
+            }
+            eventsGest.triggerEvent("selection", e, {
+                selected: data.selected
+            });
+        });
+
+        new Button(this.#canvas, 0, 2, eventsGest, {
+            "event":"eraseDigitClick",
+            "picto":"eraser"
+        });
+        eventsGest.addEvent("eraseDigitClick", function(e, data){
+            eventsGest.triggerEvent("digit", e, {
+                anchor: self.position
+            });
+        });
+
+        new Button(this.#canvas, 0, 5, eventsGest, {
+            "picto":"undo"
+        });
+
+        new Button(this.#canvas, 0, 6, eventsGest, {
+            "picto":"redo"
+        });
 
         for (let i=0; i<3; i++) {
             for (let j=0; j<3; j++) {
-                let tag = `${i*3+j+1}`;
-                (new Button(this.#canvas, i+1, j+1, tag)).drawText(tag).assignCallBack(
-                    function(b, e){
-                        board.toggleDigit(b.tag, self.position, self.selectedColor);
-                    }
-                );
+                let value = `${i*3+j+1}`;
+                new Button(this.#canvas, i+1, j+1, eventsGest, {
+                    "caption":value,
+                    "data":value,
+                    "event":"digitClick"
+                });
             }
         }
+        eventsGest.addEvent("digitClick", function(e, data){
+            eventsGest.triggerEvent("digit", e, {
+                digit: data.data,
+                anchor: self.position,
+                color: self.selectedColor
+            });
+        });
 
-        this.#radioPosition = new Radio([
-            (new Button(this.#canvas, 1, 4, "NW")).drawSquare(Button.FILLDARKER, 0.2, 0.2, 0.3),
-            (new Button(this.#canvas, 1, 5, "N")).drawSquare(Button.FILLDARKER, 0.35, 0.2, 0.3),
-            (new Button(this.#canvas, 1, 6, "NE")).drawSquare(Button.FILLDARKER, 0.5, 0.2, 0.3),
-            (new Button(this.#canvas, 2, 4, "W")).drawSquare(Button.FILLDARKER, 0.2, 0.35, 0.3),
-            (new Button(this.#canvas, 2, 5, "C")).drawSquare(Button.FILLDARKER, 0.35, 0.35, 0.3),
-            (new Button(this.#canvas, 2, 6, "E")).drawSquare(Button.FILLDARKER, 0.5, 0.35, 0.3),
-            (new Button(this.#canvas, 3, 4, "SW")).drawSquare(Button.FILLDARKER, 0.2, 0.5, 0.3),
-            (new Button(this.#canvas, 3, 5, "S")).drawSquare(Button.FILLDARKER, 0.35, 0.5, 0.3),
-            (new Button(this.#canvas, 3, 6, "SE")).drawSquare(Button.FILLDARKER, 0.5, 0.5, 0.3),
-            (new Button(this.#canvas, 0, 4, "P")).drawSquare(Button.FILLDARKER, 0.2, 0.2, 0.6)
-        ]);
+        this.#radioPosition = new Radio("position", [
+            (new Button(this.#canvas, 1, 4, eventsGest, { tag:"NW" })).drawSquare(Button.FILLDARKER, 0.2, 0.2, 0.3),
+            (new Button(this.#canvas, 1, 5, eventsGest, { tag:"N"  })).drawSquare(Button.FILLDARKER, 0.35, 0.2, 0.3),
+            (new Button(this.#canvas, 1, 6, eventsGest, { tag:"NE" })).drawSquare(Button.FILLDARKER, 0.5, 0.2, 0.3),
+            (new Button(this.#canvas, 2, 4, eventsGest, { tag:"W"  })).drawSquare(Button.FILLDARKER, 0.2, 0.35, 0.3),
+            (new Button(this.#canvas, 2, 5, eventsGest, { tag:"C"  })).drawSquare(Button.FILLDARKER, 0.35, 0.35, 0.3),
+            (new Button(this.#canvas, 2, 6, eventsGest, { tag:"E"  })).drawSquare(Button.FILLDARKER, 0.5, 0.35, 0.3),
+            (new Button(this.#canvas, 3, 4, eventsGest, { tag:"SW" })).drawSquare(Button.FILLDARKER, 0.2, 0.5, 0.3),
+            (new Button(this.#canvas, 3, 5, eventsGest, { tag:"S"  })).drawSquare(Button.FILLDARKER, 0.35, 0.5, 0.3),
+            (new Button(this.#canvas, 3, 6, eventsGest, { tag:"SE" })).drawSquare(Button.FILLDARKER, 0.5, 0.5, 0.3),
+            (new Button(this.#canvas, 0, 4, eventsGest, { tag:"P"  })).drawSquare(Button.FILLDARKER, 0.2, 0.2, 0.6)
+        ], eventsGest);
 
         let buttonsColor = [];
-        for (let i=1; i<3; i++){
-            for (let j=1; j<3; j++) {
+        for (let i=0; i<3; i++){
+            for (let j=0; j<3; j++) {
                 let cindex = i*3+j;
                 let c = COLORS[cindex];
-                let b = (new Button(this.#canvas, c)).drawSquare(c, 0.3, 0.3, 0.4);
+                let b = (new Button(this.#canvas, i+1, j+9, eventsGest, { tag:c })).drawSquare(c, 0.3, 0.3, 0.4);
                 buttonsColor.push(b);
             }
         }
-        this.#radioColor = new Radio(buttonsColor);
+        this.#radioColor = new Radio("color", buttonsColor, eventsGest);
     }
 
     /**
@@ -150,8 +171,6 @@ class Pad {
     get position() {
         return this.#radioPosition.selected;
     }
-
-
 }
 
 export { Pad };
