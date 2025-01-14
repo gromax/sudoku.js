2bdef438ed9a3843290a7c004c25085df4938534
Maxence Klein <maxence.klein@goupill.fr>
2025-01-14 23:04:10 +0100

    suffit d'appeler Game

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@
 //import { GGrid } from './graphic/grid';
 //import { Selection } from './graphic/selection';
 
-import { Board } from "./graphic/board";
-import { Pad } from "./interface/pad";
 
+import { Game } from "./game/game";
 
-
-var board = new Board('#board', '9x9S', 'ThBEBGEGED:Y;LibBIBIFgF:b;CagEFFCFB:py-;Cagffgfgh:o:20{2÷};lala;liCdEdEf:b;8FH:r;5BE:w;Tag{>}Gd:r.Cs80rR;CagJIJJIJ:gv:0=;ColBIBJCJ:v.50');
-var pad = new Pad("#pad", board);
+new Game("#board", "#pad", '9x9S', 'ThBEBGEGED:Y;LibBIBIFgF:b;CagEFFCFB:py-;Cagffgfgh:o:20{2÷};lala;liCdEdEf:b;8FH:r;5BE:w;Tag{>}Gd:r.Cs80rR;CagJIJJIJ:gv:0=;ColBIBJCJ:v.50');
 
 
 //var selector = new Selection(board);
