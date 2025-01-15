a7ab1f618c3a9d4b36e0951a9531bb2854c9546a
Maxence Klein <maxence.klein@goupill.fr>
2025-01-15 22:49:09 +0100

    gestion back/forward. Codage mis en attente.

--- src/utils/history.js
+++ src/utils/history.js
@@ -1,5 +1,6 @@
 import { letterToInt, intToLetter } from './misc';
 import { COLORS } from "../constantes"
+import { Events } from './events';
 
 const SYMBOLS = {
     "color": "_",
@@ -13,18 +14,32 @@ class History {
     #width;
     /** @type {number} */
     #height;
-    /** @type {Array<string>} */
+    /** @type {Array<Object>} */
     #liste;
+    /** @type {number} */
+    #cursor;
+    /** @type {Events} */
+    #eventsGest;
 
     /**
      * constructeur
      * @param {number} width
      * @param {number} height
      */
-    constructor(width, height){
+    constructor(width, height, eventsGest){
         this.#liste = [];
         this.#width = width;
         this.#height = height;
+        this.#cursor = 0;
+        this.#eventsGest = eventsGest;
+
+        let self = this;
+        eventsGest.addEvent("backClick", function(e,data){
+            self.back(e);
+        });
+        eventsGest.addEvent("forwardClick", function(e,data){
+            self.forward(e);
+        });
     }
 
     
@@ -41,28 +56,35 @@ class History {
         let col = index - this.#width*line;
         return [line, col];
     }
-    
+
+    /**
+     * supprime l'historique à partir du rang #cursor
+     */
+    #purge() {
+        this.#liste = this.#liste.slice(0, this.#cursor);
+    }
+
     /**
      * ajoute un changement de couleur à l'historique
      * @param {number[]} indexes 
      * @param {string} color 
      */
     pushCol(indexes, color) {
+        this.#purge();
+        this.#liste.push({type:"color", indexes:indexes, color:color});
+        this.#cursor++;
+        /*
         let code = this.#indexesToCode(indexes);
-        let icol = COLORS.indexOf[color];
-        if (icol == -1) {
-            throw new Error(`couleur ${color} invalide.`);
+        if (color == "") {
+            this.#liste.push(SYMBOLS.color + SYMBOLS.clear + code);
+        } else {
+            let icol = COLORS.indexOf[color];
+            if (icol == -1) {
+                throw new Error(`couleur ${color} invalide.`);
+            }
+            this.#liste.push(SYMBOLS.color + icol + code);
         }
-        this.#liste.push(SYMBOLS.color + icol + code);
-    }
-
-    /**
-     * Ajoute un effacement de couleur à l'historique
-     * @param {number[]} indexes 
-     */
-    pushClearCol(indexes) {
-        let code = this.#indexesToCode(indexes);
-        this.#liste.push(SYMBOLS.color + SYMBOLS.clear + code);
+        */
     }
 
     /**
@@ -72,12 +94,17 @@ class History {
      * @param {number|string} direction 
      */
     pushBorder(indexes, color, direction) {
+        this.#purge();
+        this.#liste.push({type:"border", indexes:indexes, color:color, direction:direction});
+        this.#cursor++;
+        /*
         let code = this.#indexesToCode(indexes);
         let icol = COLORS.indexOf[color];
         if (icol == -1) {
             throw new Error(`couleur ${color} invalide.`);
         }
         this.#liste.push(SYMBOLS.border + direction + icol + code);
+        */
     }
 
     /**
@@ -88,22 +115,48 @@ class History {
      * @param {string} color 
      */
     pushDigit(indexes, digit, anchor, color) {
+        this.#purge();
+        this.#liste.push({type:"digit", indexes:indexes, digit:digit, anchor:anchor, color:color});
+        this.#cursor++;
+        /*
         let code = this.#indexesToCode(indexes);
-        let icol = COLORS.indexOf[color];
-        if (icol == -1) {
-            throw new Error(`couleur ${color} invalide.`);
+        if (digit == "") {
+            this.#liste.push(anchor + SYMBOLS.clear + code);
+        } else {
+            let icol = COLORS.indexOf[color];
+            if (icol == -1) {
+                throw new Error(`couleur ${color} invalide.`);
+            }
+            this.#liste.push(''+ digit + anchor + color + code);
         }
-        this.#liste.push(''+ digit + anchor + color + code);
+        */
     }
 
     /**
-     * ajoute une suppression de digit à l'historique
-     * @param {number[]} indexes 
-     * @param {string} anchor 
+     * fait avancer d'un pas dans l'historique
+     * déclence un événement avec l'action suivante
+     * @param {Event}
      */
-    pushClearDigit(indexes, anchor) {
-        let code = this.#indexesToCode(indexes);
-        this.#liste.push(''+ digit + anchor + SYMBOLS.clear + code);
+    forward(e) {
+        if (this.#cursor>=this.#liste.length){
+            return;
+        }
+        let action = this.#liste[this.#cursor];
+        this.#cursor++;
+        this.#eventsGest.triggerEvent("forward", e, {action:action});
+    }
+
+    /**
+     * fait reculer d'un pas dans l'historique
+     * déclenche un événement contenant la liste des actions permettant de reconstruire l'état précédent
+     * @param {Event}
+     */
+    back(e) {
+        if (this.#cursor==0) {
+            return;
+        }
+        this.#cursor--;
+        this.#eventsGest.triggerEvent("back", e, {actions:this.#liste.slice(0, this.#cursor)});
     }
 
     /**
@@ -119,6 +172,43 @@ class History {
         }
         return result;
     }
+
+    /**
+     * convertit un mot d'une base à une autre
+     * @param {number} baseSource 
+     * @param {number} baseCible 
+     * @param {number[]} digits 
+     * @returns {number[]}
+     */
+    #codage(baseSource, baseCible, digits){
+        let out = [];
+        while (_.sum(digits) > 0) {
+            let [r, q] = this.#euclidian(baseSource, baseCible, digits);
+            out.push(r);
+            digits = q;
+        }
+        return out;
+    }
+
+    /**
+     * fait la division euclidienne de digits/baseCible
+     * @param {number} baseSource 
+     * @param {number} baseCible 
+     * @param {number[]} digits nombre exprimé en baseSource, poids faible = digits[0]
+     * @returns {[number, number[]]} [reste, quotient]
+     */
+    #euclidian(baseSource, baseCible, digits) {
+        let out = [];
+        let r = 0;
+        for (let i=digits.length - 1; i>=0; i--) {
+            let d = digits[i] + r*baseSource;
+            let q = Math.floor(d/baseCible);
+            r = d % baseCible;
+            out.push(q);
+        }
+        return [r, out.reverse()];
+    }
+
 
 
 
