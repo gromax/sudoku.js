241bc9512ca2c67b491157a87e401da47b1eecc6
Maxence Klein <maxence.klein@goupill.fr>
2025-01-15 22:47:40 +0100

    ajout code cardinaux pour historique

--- src/constantes.js
+++ src/constantes.js
@@ -10,15 +10,15 @@ const DIRECTION = {
  }
 
 const ANCRES = {
-    "N" : {x:0.5, y:0,   clockwise:"E",  counterclockwise:"W" },
-    "NE": {x:1,   y:0,   clockwise:"SE", counterclockwise:"NW"},
-    "NW": {x:0,   y:0,   clockwise:"NE", counterclockwise:"SW"},
-    "E" : {x:1,   y:0.5, clockwise:"S",  counterclockwise:"N" },
-    "SE": {x:1,   y:1,   clockwise:"SW", counterclockwise:"NE"},
-    "S" : {x:0.5, y:1,   clockwise:"W",  counterclockwise:"E" },
-    "SW": {x:0,   y:1,   clockwise:"NW", counterclockwise:"SE"},
-    "W" : {x:0,   y:0.5, clockwise:"N",  counterclockwise:"S" },
-    "C" : {x:0.5, y:0.5, clockwise:"C",  counterclockwise:"C" },
+    "N" : {x:0.5, y:0,   clockwise:"E",  counterclockwise:"W",  code:4},
+    "NE": {x:1,   y:0,   clockwise:"SE", counterclockwise:"NW", code:7},
+    "NW": {x:0,   y:0,   clockwise:"NE", counterclockwise:"SW", code:1},
+    "E" : {x:1,   y:0.5, clockwise:"S",  counterclockwise:"N",  code:8},
+    "SE": {x:1,   y:1,   clockwise:"SW", counterclockwise:"NE", code:9},
+    "S" : {x:0.5, y:1,   clockwise:"W",  counterclockwise:"E",  code:6},
+    "SW": {x:0,   y:1,   clockwise:"NW", counterclockwise:"SE", code:3},
+    "W" : {x:0,   y:0.5, clockwise:"N",  counterclockwise:"S",  code:2},
+    "C" : {x:0.5, y:0.5, clockwise:"C",  counterclockwise:"C",  code:5},
 }
 
 const COLORS = [
