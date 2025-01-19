42a62a70d0a6752e0eaeeae12f20aa4c0f9ab050
Maxence Klein <maxence.klein@goupill.fr>
2025-01-19 23:35:19 +0100

    premier commit

---
+++ src/interface/saisie.js
@@ -0,0 +1,73 @@
+/**
+ * gère la zone de saisie de texte
+ */
+
+import { Events } from "../utils/events";
+
+const DIVID = "saisie";
+
+class Saisie {
+    #node;
+    /** @type {string} */
+    #name;
+    /** @type {Events} */
+    #eventsGest;
+
+    /**
+     * constructeur
+     * @param {string} name
+     * @param {Events} eventsGest 
+     */
+    constructor(name, eventsGest) {
+        this.#node = document.getElementById(DIVID);
+        this.#name = name;
+        this.#eventsGest = eventsGest;
+    }
+
+    /**
+     * accesseur name
+     * @returns {string}
+     */
+    get name() {
+        return this.#name;
+    }
+
+    /**
+     * affiche le champ de saisie
+     * @param {string} text 
+     */
+    show(text){
+        if (!this.#node) {
+            return;
+        }
+        this.#node.innerHTML = "";
+        let form = document.createElement("form");
+        let textarea = document.createElement("textarea");
+        textarea.value = text;
+        textarea.placeholder = "Entrez un commentaire."
+        let inp = document.createElement("input");
+        inp.type = "submit";
+        inp.value = "valider";
+        form.appendChild(textarea);
+        form.appendChild(inp);
+        let self = this;
+        let evG = this.#eventsGest
+        form.addEventListener("submit", function(e){
+            evG.triggerEvent("submitSaisie", e, {
+                name:self.name,
+                text:textarea.value
+             })
+            e.preventDefault();
+        });
+        this.#node.appendChild(form);
+    }
+
+    hide() {
+        if (this.#node) {
+            this.#node.innerHTML = "";
+        }
+    }
+
+}
+
+export {Saisie}
