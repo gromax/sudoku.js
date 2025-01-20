f366d9aaf01659133510b96998921fa771d914c3
Maxence Klein <maxence.klein@goupill.fr>
2025-01-20 23:32:15 +0100

    production d'un module autonome GameModule

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,13 @@ const path = require('path');
 var webpack = require('webpack');
 module.exports = {
 	mode: "development",
-	entry: {
-		index:'./src/index.js'
-	},
+	entry: './src/game/game.js',
 	output: {
-		filename: '[name].js',
+		filename: 'game.js',
 		path: path.resolve(__dirname, 'dist'),
+		library: 'GameModule', // Nom de la bibliothèque (optionnel, utile pour UMD)
+    	libraryTarget: 'umd', // Cible pour rendre le module universel (UMD)
+    	globalObject: 'this', // Assure la compatibilité pour Node.js et le navigateur
 	},
 	plugins: [
 		new webpack.ProvidePlugin({
